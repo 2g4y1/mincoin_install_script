23b4bdf42d6f7cd9bdeded5f82ecdfdcb3f0a3b3
2g4y1 <0043664@gmail.com>
2018-01-08 21:39:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,10 +13,10 @@ echo "deb-src http://deb.torproject.org/torproject.org xenial main" | sudo tee -
 gpg --keyserver keys.gnupg.net --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 apt update
-apt install tor deb.torproject.org-keyring -y
+sudo apt install tor deb.torproject.org-keyring -y
 
 #Autostart Tor
-sed -i 's/#RunAsDaemon/RunAsDaemon/g' /etc/tor/torrc
+sudo sed -i 's/#RunAsDaemon/RunAsDaemon/g' /etc/tor/torrc
 echo "HiddenServiceDir /var/lib/tor/hidden_service/" | sudo tee -a /etc/tor/torrc
 echo "HiddenServicePort 9334 127.0.0.1:9334" | sudo tee -a /etc/tor/torrc
 systemctl restart tor
@@ -35,8 +35,8 @@ USER=$(head -c 16  /dev/random | md5sum | cut -f 1 -d\ )
 
 echo "rpcuser=1234" | sudo tee -a /root/.mincoin/mincoin.conf
 echo "rpcpassword=5678" | sudo tee -a /root/.mincoin/mincoin.conf
-sed -i "s/1234/$USER/g" /root/.mincoin/mincoin.conf
-sed -i "s/4567/$PASSWORD/g" /root/.mincoin/mincoin.conf
+sudo sed -i "s/1234/$USER/g" /root/.mincoin/mincoin.conf
+sudo sed -i "s/4567/$PASSWORD/g" /root/.mincoin/mincoin.conf
 mincoin-0.8.8.0-linux/bin/64/mincoind
 systemctl stop mincoind
 wget "https://github.com/mincoin/mincoin/releases/download/v0.8.8.0/Mincoin-1553460-bootstrap.dat"
