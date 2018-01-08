8c02a8b1a637c7738f54f9b44d3bfaae04f432c5
2g4y1 <0043664@gmail.com>
2018-01-08 20:25:00 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+#+++++++++++++++++++#
+#Installation of Tor#
+#+++++++++++++++++++#
+
+echo "deb http://deb.torproject.org/torproject.org xenial main" | sudo tee -a /etc/apt/sources.list
+echo "deb-src http://deb.torproject.org/torproject.org xenial main" | sudo tee -a /etc/apt/sources.list
+gpg --keyserver keys.gnupg.net --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89
+gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
+apt update
+apt install tor deb.torproject.org-keyring -y
+
+#Autostart Tor
+sed -i 's/#RunAsDaemon/RunAsDaemon/g' /etc/tor/torrc
+echo "HiddenServiceDir /var/lib/tor/hidden_service/" | sudo tee -a /etc/tor/torrc
+echo "HiddenServicePort 9334 127.0.0.1:9334" | sudo tee -a /etc/tor/torrc
+
+#+++++++++++++++++++#
+#Installation of MNC#
+#+++++++++++++++++++#
+
+wget "https://github.com/mincoin/mincoin/releases/download/v0.8.8.0/mincoin-0.8.8.0-linux.tar.gz"
+tar xf mincoin-0.8.8.0-linux.tar.gz -C /mincoin
+wget "https://github.com/mincoin/mincoin/releases/download/v0.8.8.0/Mincoin-1553460-bootstrap.dat"
+mv Mincoin-1553460-bootstrap.dat /root/.mincoin/bootstrap.dat
+
+
+external="`cat /var/lib/tor/hidden_service/hostname`"
+external="externalip=$external
+$external > /root/.mincoin/mincoin.conf
+"timeout=750" > /root/.mincoin/mincoin.conf
+"tor=127.0.0.1:9050" > /root/.mincoin/mincoin.conf
+"discover=1" > /root/.mincoin/mincoin.conf
+"upnp=1" > /root/.mincoin/mincoin.conf
+"txindex=1" > /root/.mincoin/mincoin.conf
+
+#Autostart Mincoin
+echo "$PWD/mincoin-0.8.8.0-linux/bin/64/mincoind -daemon" | sudo tee -a /etc/rc.local
