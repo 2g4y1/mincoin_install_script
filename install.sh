76318010ca8d527d693c986be0d89633dc66f2ed
2g4y1 <0043664@gmail.com>
2018-01-08 21:22:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
 
 #+++++++++++++++++++#
 #Installation of Tor#
@@ -30,10 +33,10 @@ touch /root/.mincoin/mincoin.conf
 PASSWORD=$(head -c 32  /dev/random | md5sum | cut -f 1 -d\ )
 USER=$(head -c 16  /dev/random | md5sum | cut -f 1 -d\ )
 
-"rpcuser=1234" > /root/.mincoin/mincoin.conf
-"rpcpassword=5678" > /root/.mincoin/mincoin.conf
-sed -i "s/#1234/$USER/g" /root/.mincoin/mincoin.conf
-sed -i "s/#4567/$PASSWORD/g" /root/.mincoin/mincoin.conf
+echo "rpcuser=1234" | sudo tee -a /root/.mincoin/mincoin.conf
+echo "rpcpassword=5678" | sudo tee -a /root/.mincoin/mincoin.conf
+sed -i "s/1234/$USER/g" /root/.mincoin/mincoin.conf
+sed -i "s/4567/$PASSWORD/g" /root/.mincoin/mincoin.conf
 
 systemctl stop mincoind
 wget "https://github.com/mincoin/mincoin/releases/download/v0.8.8.0/Mincoin-1553460-bootstrap.dat"
@@ -42,12 +45,13 @@ mv Mincoin-1553460-bootstrap.dat /root/.mincoin/bootstrap.dat
 
 external="`cat /var/lib/tor/hidden_service/hostname`"
 external="externalip=$external
-$external > /root/.mincoin/mincoin.conf
-"timeout=750" > /root/.mincoin/mincoin.conf
-"tor=127.0.0.1:9050" > /root/.mincoin/mincoin.conf
-"discover=1" > /root/.mincoin/mincoin.conf
-"upnp=1" > /root/.mincoin/mincoin.conf
-"txindex=1" > /root/.mincoin/mincoin.conf
+echo "timeout=750" | sudo tee -a /root/.mincoin/mincoin.conf
+echo $external | sudo tee -a /root/.mincoin/mincoin.conf
+echo "tor=127.0.0.1:9050" | sudo tee -a /root/.mincoin/mincoin.conf
+echo "discover=1" | sudo tee -a /root/.mincoin/mincoin.conf
+echo "upnp=1" | sudo tee -a /root/.mincoin/mincoin.conf
+echo "txindex=1" | sudo tee -a /root/.mincoin/mincoin.conf
+
 
 #Autostart Mincoin
 echo "/root/mincoin-0.8.8.0-linux/bin/64/mincoind -daemon" | sudo tee -a /etc/rc.local
