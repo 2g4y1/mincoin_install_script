009090bcf1a5633e682e195571924580e1c25851
2g4y1 <0043664@gmail.com>
2018-02-25 11:28:48 +0100

    Create first.sh

---
+++ first.sh
@@ -0,0 +1,14 @@
+#add user mincoin
+adduser mincoin
+
+#now give this user sudo access (more power!)
+usermod -aG sudo mincoin
+
+#switch to that user:
+su - mincoin
+cd ~/
+
+#now download and run the install script:
+wget https://raw.githubusercontent.com/2g4y1/mincoin_install_script/master/install.sh
+sudo chmod +x saga_install.sh
+sh saga_install.sh
